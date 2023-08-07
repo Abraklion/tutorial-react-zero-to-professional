a3aed34081efb22f31d52629661e09d68de95d8a
Abraklion <abraklion@gmail.com>
2023-08-07 16:15:52 +0500

    Блок 3. Теория. Основы React | Урок 6. Работа со списком (циклы)

--- block_3/my-app/src/Car/Car.js
+++ block_3/my-app/src/Car/Car.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default props => (
+const Car = props => (
   <div>
     <h3>Сar name: {props.name}</h3>
     <p>Year: <strong>{props.year}</strong></p>
     <button onClick={props.onChangeTitle}>Click</button>
   </div>
 )
+
+export default Car
