746aeda0c05808ac7bc89328e70ce48061b144d8
Abraklion <abraklion@gmail.com>
2023-08-07 18:31:31 +0500

    Блок 3. Теория. Основы React | Урок 7. Работа с условными операторами

--- block_3/my-app/src/App.js
+++ block_3/my-app/src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
       {name: 'Audi', year: 2016},
       {name: 'Mazda', year: 2010}
     ],
-    pageTitle: 'React components'
+    pageTitle: 'React components',
+    showCars: false
   }
 
   /**
@@ -38,12 +39,11 @@ class App extends Component {
 
   }
 
-  render() {
+  // шаблон 1
+  templateOne () {
     const divStyle = {
       textAlign: 'center'
     }
-
-    // const cars = this.state.cars
 
     return (
       <div style={divStyle}>
@@ -87,6 +87,59 @@ class App extends Component {
         {/*/>*/}
       </div>
     );
+  }
+
+
+  toggleCarsHandler = () => {
+
+    console.log(this.state.showCars)
+
+    this.setState({
+      showCars: !this.state.showCars
+    })
+  }
+
+  // шаблон 2
+  templateTwo () {
+    const divStyle = {
+      textAlign: 'center'
+    }
+
+    let cars = null
+
+    /**
+     * Урок 7. Работа с условными операторами
+     **/
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+          <Car
+            key={index}
+            name={car.name}
+            year={car.year}
+            onChangeTitle={() => this.changeTitleHandler(car.name)}
+          />
+        )
+      })
+    }
+
+    return (
+      <div style={divStyle}>
+        <h1>{this.state.pageTitle}</h1>
+
+        <button
+          onClick={this.toggleCarsHandler}
+        >Toggle cars</button>
+
+        { cars }
+      </div>
+    );
+  }
+
+  render() {
+
+    return this.templateTwo()
+
   }
 }
 
