32e77888aac4dd1c43cf32407769b54b3b82b46f
Abraklion <abraklion@gmail.com>
2023-08-19 13:26:15 +0500

    Блок 6. Практика. Компоненты | Урок 14. Кнопка переключения меню

--- block_6/src/hoc/Layout/index.js
+++ block_6/src/hoc/Layout/index.js
@@ -1,6 +1,7 @@
 import {Component} from "react";
 import style from './Layout.module.scss'
 
+import MenuToggle from "../../components/Navigation/MenuToggle";
 
 /**
  * Компонент {Викторина}
@@ -8,11 +9,39 @@ import style from './Layout.module.scss'
 class Layout extends Component {
 
   /**
+   * Состояние компонента
+   **/
+  state = {
+
+    // открывает / закрывает меню
+    menu: false
+  }
+
+
+  /**
+   * Обработчик клика по бургер меню
+   * @param {Object=} e - обьект события
+   *
+   * @return {void}
+   */
+  toggleMenuHandler = (e) => {
+    this.setState({
+      menu: !this.state.menu
+    })
+  }
+
+  /**
    * Отрисовка
    **/
   render() {
     return (
       <>
+        <header>
+          <MenuToggle
+            onToggle={this.toggleMenuHandler}
+            isOpen={this.state.menu}
+          />
+        </header>
         <main className={style.Layout}>
           {this.props.children}
         </main>
